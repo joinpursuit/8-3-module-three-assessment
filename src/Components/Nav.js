a56126922f4259828759e39a79d7418f47b31a33
arupay <arupay11374@gmail.com>
2022-06-24 11:18:17 -0400

    created and linked navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
 const Nav = (props) => {
-  return <div>Here is the Nav Bar</div>;
+  return (
+    <div className="nav">
+      <Link to="/">
+        <img
+          className="icon"
+          src="https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/256x256/plain/movies.png"
+          alt=""
+        />
+      </Link>
+      <Link to="/movies">
+        <div>Movies</div>
+      </Link>
+      <Link to="/people">
+        <div>People</div>
+      </Link>
+      <Link to="/locations">
+        <div>Locations</div>
+      </Link>
+    </div>
+  );
 };
 
 export default Nav;
