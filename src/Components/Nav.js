0b0d85594b8a963474baf595fc5be144f84a135f
Charlie Moran <Carlosamoran18@gmail.com>
2022-06-24 12:02:28 -0400

    Nav javascript done

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,12 +1,27 @@
 import "./Styles/Nav.css"
 import React, {Component} from "react";
+import logo from "../Images/logo.png";
+import {Link} from "react-router-dom";
 
 class Nav extends Component{
     render(){
         return(
-            <div>
-
+            <nav>
+            <div id="logo-container">
+                <Link className="logo" to="/">
+                    <img src={logo} alt="logo" className="ghibli-logo" />
+                </Link>
+                </div>
+                <div id="leMovies">
+					<Link to="/Movies">Movies</Link>
+					<div id="lePeople">
+					<Link to="/People">People</Link>
+					</div>
+					<div id="leLocations">
+					<Link to="/Locations">Locations</Link>
+					</div>
             </div>
+            </nav>
         )
     }
 }
