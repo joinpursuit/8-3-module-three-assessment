eba1e32eb292b9149585ad09440cefa619bddb0c
Charlie Moran <Carlosamoran18@gmail.com>
2022-06-24 10:27:53 -0400

    Outline

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Component } from 'react';
+import Nav from "./Components/Nav";
+import Home from "./Components/Home";
+import Movies from "./Components/Movies";
+import People from "./Components/People";
+import Locations from "./Components/Locations";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+        <Route exact path="/" component={Home} />
+        <Route path="/Movies" component={Movies} />
+        <Route path="/People" component={People} />
+        <Route path="/Locations" component={Locations} />
+        </Routes>
+      </div>
+      </BrowserRouter>
+    )
+  }
 }
-
 
 export default App;
