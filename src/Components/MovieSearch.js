6d09482eba10c99f51d3ecc23502884b81133c8f
Charlie Moran <Carlosamoran18@gmail.com>
2022-06-24 10:49:53 -0400

    movie search setup

--- src/Components/MovieSearch.js
+++ src/Components/MovieSearch.js
@@ -1,1 +1,17 @@
 import "./Styles/MovieSearch.css"
+import React, {Component} from "react";
+
+class MovieSearch extends Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default MovieSearch
