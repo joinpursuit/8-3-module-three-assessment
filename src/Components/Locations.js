bf57dc7d7974819e0548571cdd4c6df8c437d1db
Charlie Moran <Carlosamoran18@gmail.com>
2022-06-24 12:16:13 -0400

    preparing to fetch

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -4,7 +4,40 @@ import React, {Component} from "react";
 class Locations extends Component {
     constructor(){
         super();
+        this.state = {
+            fetchdata: [],
+            toggle: false,
+        }
     }
+
+    searchy = (evt) => {
+        this.setState({searchyValue: e.target.value,});
+    };
+    
+    componentDidMount() {
+		fetch("https://ghibliapi.herokuapp.com/locations")
+			.then((res) =>  res.json())
+			.then((data) => {
+				data.forEach((location) => {
+					this.setState({
+						fetchdata: [
+						...this.state.fetchdata,
+						<li key={location.name}>
+							<p>Name: {location.name}</p>
+							<p>Climate: {location.climate}</p>
+							<p>Terrain: {location.terrain}</p>
+						</li>,
+						],
+					});
+				})
+				
+			});
+	};
+
+	hide = () => {
+		this.setState({toggle: !this.state.toggle})
+	}
+
     render(){
         return (
             <div>
