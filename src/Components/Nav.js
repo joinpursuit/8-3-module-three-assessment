9973082f5fd971f890d7de732a41fc9b7e1e0b8d
Charlie Moran <Carlosamoran18@gmail.com>
2022-06-24 10:52:22 -0400

    Nav setup

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,1 +1,14 @@
 import "./Styles/Nav.css"
+import React, {Component} from "react";
+
+class Nav extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Nav
