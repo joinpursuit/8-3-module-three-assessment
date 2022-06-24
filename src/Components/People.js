c4775eaeabc129c449c0f3b5af6bf51188220996
Charlie Moran <Carlosamoran18@gmail.com>
2022-06-24 10:53:33 -0400

    People setup

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,1 +1,17 @@
 import "./Styles/People.css"
+import React, {Component} from "react";
+
+class People extends Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default People
