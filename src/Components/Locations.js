74bbe09cb6cfdd47d78555cf2a7b7cd6f3c8ece6
Charlie Moran <Carlosamoran18@gmail.com>
2022-06-24 10:43:41 -0400

    locations setup

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -1,1 +1,17 @@
 import "./Styles/Locations.css"
+import React, {Component} from "react";
+
+class Locations extends Component {
+    constructor(){
+        super();
+    }
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Locations
