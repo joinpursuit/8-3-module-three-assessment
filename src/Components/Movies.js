df52b30eb6f24a77d6b6ede7872f674a3c312f57
Charlie Moran <Carlosamoran18@gmail.com>
2022-06-24 10:47:12 -0400

    movies setup

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,1 +1,17 @@
 import "./Styles/Movies.css"
+import React, {Component} from "react";
+
+class Movies extends Component {
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Movies
