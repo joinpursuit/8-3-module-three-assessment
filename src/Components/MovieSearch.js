37f4a3861bb70cfcdaca28cb9182cedc4659caba
Charlie Moran <Carlosamoran18@gmail.com>
2022-06-24 15:00:54 -0400

    Movie Search done

--- src/Components/MovieSearch.js
+++ src/Components/MovieSearch.js
@@ -2,16 +2,33 @@ import "./Styles/MovieSearch.css"
 import React, {Component} from "react";
 
 class MovieSearch extends Component{
-    constructor(){
-        super();
-    }
-    render(){
-        return(
-            <div>
+	constructor(){
+		super();
+		this.state = {
+			films: "",
+		};
+	}
 
-            </div>
-        )
-    }
+componentDidMount() {
+		let film = [];
+		fetch(`curl https://ghibliapi.herokuapp.com/films/`)
+		.then(response => {
+			return response.json();
+		}).then(data => {
+			film = data.results.map((title) => {
+				return title;
+			});
+			this.setState({
+				films: film,
+			});
+		});
 }
 
-export default MovieSearch
+render() {
+		return (
+			<title state={this.state} />
+		)
+	}
+}
+
+export default MovieSearch;
