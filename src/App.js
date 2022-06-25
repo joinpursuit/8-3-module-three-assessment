4986d4ac9d1dfea7034ad8bc4796c999340c488e
Andres Varon <anvaron@gmail.com>
2022-06-25 10:32:44 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 import logo from './logo.svg';
 import './App.css';
@@ -6,18 +9,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
+        <Home />
       </header>
 
     </div>
