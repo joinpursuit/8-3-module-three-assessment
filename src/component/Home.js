21221f5b013e7638b80a6dd0f8bd4517359013bb
Lortiz528 <lortiztech@gmail.com>
2022-06-24 23:56:46 -0400

    home image

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import './home.css';
+import "./home.css"
 
 class Home extends React.Component {
   render() {
     return (
-      <div className="home">
+        <div className='home'>
         <h1>Welcome to GhibliApp</h1>
-        <img
-          src="https://media3.giphy.com/media/nthoYgQ91Up2u7qmcE/200.webp?cid=ecf05e47auili7jgvu09gb08q3k42dy4lg7f0x3gs6ahhcgm&rid=200.webp&ct=g"
-          alt="ghibly gif"
-        />
-      </div>
-    );
+        <img src='https://media3.giphy.com/media/liUhPmZdArpYc/giphy.gif' alt='ghibly gif'/>
+        </div>
+    )
   }
 }
 
