b4084f6c8be33c6eb341c526656e7100e24727b4
Edwin Codrington <edwincodrington@gmail.com>
2022-06-24 22:50:58 -0400

    locations populated, sort implementation to follow

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -1,7 +1,84 @@
 import React, { Component } from "react";
-
+import "./Locations.css";
 export default class Locations extends Component {
+	constructor() {
+		super();
+		this.state = {
+			show: false,
+			locations: [],
+		};
+	}
+	showLocations = () => {
+		this.setState({ show: !this.state.show });
+	};
+	componentDidMount() {
+		const { BASE_URL } = this.props;
+		fetch(`${BASE_URL}/locations`)
+			.then((response) => response.json())
+			.then((obj) => {
+				this.setState({ locations: [...obj] });
+			});
+	}
+	sortByName = () => {
+		let temp = this.state.locations.sort();
+		this.setState({
+			locations: [...temp],
+		});
+	};
+	sortByClimate = () => {
+		let temp = this.state.locations.sort((a, b) => {
+			if (a.climate.toLowerCase() < b.climate.toLowerCase()) {
+				return -1;
+			} else if (a.climate.toLowerCase() > b.climate.toLowerCase()) {
+				return 1;
+			} else {
+				return 0;
+			}
+		});
+		this.setState({
+			locations: [...temp],
+		});
+	};
+	sortByLocation = () => {
+		let temp = this.state.locations.sort((a, b) => {
+			return a.location.toLowerCase() - b.location.toLowerCase();
+		});
+		this.setState({
+			locations: [...temp],
+		});
+	};
 	render() {
-		return <div>Locations</div>;
+		let sortButtons = (
+			<div>
+				<button onClick={this.sortByName}>Sort by Name</button>
+				<button onClick={this.sortByClimate}>Sort by Climate</button>
+				<button onClick={this.sortByLocation}>Sort by Location</button>
+			</div>
+		);
+		return (
+			<div className="locations">
+				<h1>List of Locations</h1>
+				<button onClick={this.showLocations}>
+					{this.state.show ? `Hide Locations` : `Show Locations`}
+				</button>
+				{this.state.show && sortButtons}
+				<div>
+					<ul className="top">
+						{this.state.show &&
+							this.state.locations.map((location) => {
+								return (
+									<li key={location.id}>
+										<ul>
+											<li>Name: {location.name}</li>
+											<li>Climate: {location.climate}</li>
+											<li>Terrain: {location.terrain}</li>
+										</ul>
+									</li>
+								);
+							})}
+					</ul>
+				</div>
+			</div>
+		);
 	}
 }
