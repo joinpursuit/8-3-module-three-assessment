204520ee6b195ca92a9e12f775292b698e09355a
Andres Varon <anvaron@gmail.com>
2022-06-25 15:09:35 -0500

    test-01: Has a Home Page - tested

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Movies from './components/Movies';
+import People from './components/People';
+import Locations from './components/Locations';
 
 import logo from './logo.svg';
 import './App.css';
@@ -8,11 +12,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <Home />
-      </header>
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Movies" element={<Movies />} />
+        <Route path="/People" element={<People />} />
+        <Route path="/Locations" element={<Locations />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/People.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function People () {
+
+  return (
+    <>
+      <h1>People</h1>
+    </>
+  )
+}
+
+export default People;
