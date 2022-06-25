718dbed54a503f49a7eb3190ae13417d898e684a
Edwin Codrington <edwincodrington@gmail.com>
2022-06-24 21:22:26 -0400

    Movies.js implemented

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import Movies from "./Components/Movies";
 import Locations from "./Components/Locations";
 import People from "./Components/People";
 
+const BASE_URL = `https://ghibliapi.herokuapp.com`;
+
 function App() {
 	return (
 		<div className="App">
 			<NavBar />
-
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/movies" element={<Movies />} />
-				<Route path="/locations" element={<Locations />} />
-				<Route path="/people" element={<People />} />
+				<Route path="/movies" element={<Movies BASE_URL={BASE_URL} />} />
+				<Route path="/locations" element={<Locations BASE_URL={BASE_URL} />} />
+				<Route path="/people" element={<People BASE_URL={BASE_URL} />} />
 			</Routes>
 		</div>
 	);
