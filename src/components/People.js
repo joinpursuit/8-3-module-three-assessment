bfb48e68ab6d76d4ecd414da90a0e32486f1c3a7
Andres Varon <anvaron@gmail.com>
2022-06-26 18:53:20 -0500

    Test-03: When a new option is selected from the dropdown, the movie`s title, release date, and description should appear underneath the dropdown. At least the title of the movie should be in some kind of heading. - tested

--- src/components/People.js
+++ src/components/People.js
@@ -1,4 +1,11 @@
 import React from "react";
+import { Container, Box } from "@mui/material";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
 
 import axios from "axios";
 
@@ -32,44 +39,44 @@ class People extends React.Component {
 
   render () {
     return (
-      // <Container maxWidth="md" className="movies">
-      //   <h1>Movies</h1>
-      //   <Box 
-      //     component="form"
-      //     sx={{
-      //       '& > :not(style)': { m: 1, width: '50ch' },
-      //     }}
-      //     noValidate
-      //     autoComplete="off"
-      //   >
-      //   <FormControl fullWidth>
-      //     <label id="demo-simple-select-label">Select a Movie</label>
-      //     <Select
-      //       labelId="demo-simple-select-label"
-      //       id="demo-simple-select"
-      //       value={this.state.movieSelected}
-      //       name="movie-select"
-      //       onChange={this.handleChange}
-      //     >
-      //       <MenuItem key='0' value='' ></MenuItem>
-      //       {this.state.movieList.map(movie =>  {
-      //         return  <MenuItem key={movie.id} 
-      //                           value={movie.id}>{movie.title}
-      //                 </MenuItem>  
-      //       })}
-      //     </Select>
-      //   </FormControl>
-      // </Box>
-      // {(this.state.isSelected) ? 
-      //   <Stack spacing={2} className="movie__info">
-      //     <MovieInfo 
-      //       movieList={this.state.movieList} 
-      //       movieSelected={this.state.movieSelected}
-      //     />
-      //   </Stack>
-      //   : null
-      // }
-      // </Container>
+      <Container maxWidth="md" className="people">
+        <h1>People</h1>
+        <Box 
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '50ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+        <FormControl fullWidth>
+          <label id="demo-simple-select-label">Select a Movie</label>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={this.state.movieSelected}
+            name="movie-select"
+            onChange={this.handleChange}
+          >
+            <MenuItem key='0' value='' ></MenuItem>
+            {this.state.movieList.map(movie =>  {
+              return  <MenuItem key={movie.id} 
+                                value={movie.id}>{movie.title}
+                      </MenuItem>  
+            })}
+          </Select>
+        </FormControl>
+      </Box>
+      {(this.state.isSelected) ? 
+        <Stack spacing={2} className="movie__info">
+          {/* <MovieInfo 
+            movieList={this.state.movieList} 
+            movieSelected={this.state.movieSelected}
+          /> */}
+        </Stack>
+        : null
+      }
+      </Container>
     )
   }
 }
