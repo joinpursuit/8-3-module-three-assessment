4295bc2e23199505e0971ad5dd088a56d8cd738a
Andres Varon <anvaron@gmail.com>
2022-06-26 18:49:09 -0500

    Test-03: Has a `select` dropdown that includes an "empty" option element followed by all of the films produced by Studio Ghibli. - tested

--- src/components/People.js
+++ src/components/People.js
@@ -1,12 +1,77 @@
 import React from "react";
 
-function People () {
+import axios from "axios";
 
-  return (
-    <>
-      <h1>People</h1>
-    </>
-  )
+class People extends React.Component {
+  constructor () {
+    super ();
+    this.state = {
+      movieList: [],
+      movieSelected: '',
+      isSelected: false,
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://ghibliapi.herokuapp.com/films').then((result) => {
+      this.setState({ movieList: result.data });
+    });
+  }
+  
+  handleChange = (event) => {
+    const {value} = event.target;
+    //
+    if(value !== '') {
+      this.setState({movieSelected: value});
+      this.setState({isSelected: true});
+    } else {
+      this.setState({movieSelected: ''});
+      this.setState({isSelected: false});  
+    }
+  };
+
+  render () {
+    return (
+      // <Container maxWidth="md" className="movies">
+      //   <h1>Movies</h1>
+      //   <Box 
+      //     component="form"
+      //     sx={{
+      //       '& > :not(style)': { m: 1, width: '50ch' },
+      //     }}
+      //     noValidate
+      //     autoComplete="off"
+      //   >
+      //   <FormControl fullWidth>
+      //     <label id="demo-simple-select-label">Select a Movie</label>
+      //     <Select
+      //       labelId="demo-simple-select-label"
+      //       id="demo-simple-select"
+      //       value={this.state.movieSelected}
+      //       name="movie-select"
+      //       onChange={this.handleChange}
+      //     >
+      //       <MenuItem key='0' value='' ></MenuItem>
+      //       {this.state.movieList.map(movie =>  {
+      //         return  <MenuItem key={movie.id} 
+      //                           value={movie.id}>{movie.title}
+      //                 </MenuItem>  
+      //       })}
+      //     </Select>
+      //   </FormControl>
+      // </Box>
+      // {(this.state.isSelected) ? 
+      //   <Stack spacing={2} className="movie__info">
+      //     <MovieInfo 
+      //       movieList={this.state.movieList} 
+      //       movieSelected={this.state.movieSelected}
+      //     />
+      //   </Stack>
+      //   : null
+      // }
+      // </Container>
+    )
+  }
 }
 
 export default People;
