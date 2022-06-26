fd7bfe65de2a495b33d7afd54cf7d64b885d75bd
Jonathan Scheiber <jon@scheiber.me>
2022-06-26 12:18:36 -0400

    update app and styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import NavBar from "./Components/NavBar";
+import Movies from "./Components/Movies";
+import Locations from "./Components/Locations";
+import People from "./Components/People";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
+    <div className="ghibliapp">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/locations" element={<Locations />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 
 export default App;
