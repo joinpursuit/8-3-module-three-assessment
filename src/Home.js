6e8826d118cc7c80850de96b007982c3ac50b576
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-06-27 15:04:31 -0400

    Some Css
    Added Css.

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,7 @@ import React from "react";
 import Css from "./Home.css";
 // import logo from "/Users/candace/Desktop/Pursuit Labs/Mod 3 Assessment/src/ghibli-icon.jpeg";
 
-
-
-const Home = ( ) => {
-    return (
-       <h1>Welcome to Ghibli App!</h1>
-    )
-    }
+const Home = () => {
+  return <h1>Welcome to Ghibli App!</h1>;
+};
 export default Home;
--- src/Movie.js
+++ src/Movie.js
@@ -1,18 +1,17 @@
 import React, { isValidElement } from "react";
-import Css from "./Movie.css"
-
+import Css from "./Movie.css";
 
 class Movies extends React.Component {
   constructor() {
     super();
     this.state = {
       movies: [],
-      chosenMovie: '',
+      selectedMovie: "",
     };
   }
 
   GetMovies = () => {
-    fetch('https://ghibliapi.herokuapp.com/films')
+    fetch("https://ghibliapi.herokuapp.com/films")
       .then((result) => result.json())
       .then((data) => {
         this.setState({
@@ -28,43 +27,42 @@ class Movies extends React.Component {
 
   selectHandler = (event) => {
     this.setState({
-      chosenMovie: event.target.value,
+      selectedMovie: event.target.value,
     });
   };
 
   render() {
-    let { movies, chosenMovie } = this.state;
+    let { movies, selectedMovie } = this.state;
 
-    let movieChoices = movies.map((movie) => {
+    let moviesList = movies.map((movie) => {
       return <option key={movie.id}>{movie.title}</option>;
     });
 
-    let selected = movies.find((movie) => movie.title === chosenMovie);
+    let selected = movies.find((movie) => movie.title === selectedMovie);
 
     return (
       <body id="movies-section">
-      <div className="movies">
-        <h1>Select a movie</h1>
-        <select onChange={this.selectHandler}>
-          <option>Select</option>
-           {movieChoices}
-         
-        </select>
-        <div>
-          {selected ? (
-            <div>
-              <h2>{selected.title}</h2>
-              <img id="movie-img" src={selected.image} alt="moviePic" />
-              <p>
-                <strong>Release Date:</strong> {selected.release_date}
-              </p>
-              <p>
-                <strong>Description:</strong> {selected.description}
-              </p>
-            </div>
-          ) : null}
+        <div className="movies">
+          <h1>Select a movie</h1>
+          <select onChange={this.selectHandler}>
+            <option>Select</option>
+            {moviesList}
+          </select>
+          <div>
+            {selected ? (
+              <div>
+                <h2>{selected.title}</h2>
+                <img id="movie-img" src={selected.image} alt="moviePic" />
+                <p>
+                  <strong>Release Date:</strong> {selected.release_date}
+                </p>
+                <p>
+                  <strong>Description:</strong> {selected.description}
+                </p>
+              </div>
+            ) : null}
+          </div>
         </div>
-      </div>
       </body>
     );
   }
