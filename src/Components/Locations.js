2f9a10b8d2eadb74f4bdc80ba7dc650394844ff1
Charlie Moran <Carlosamoran18@gmail.com>
2022-06-27 10:12:52 -0400

    push for me

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -33,12 +33,14 @@ class Locations extends Component {
     render(){
         let allLoco = this.state.allLocations.map((each) => {
             return (
+                <div>
                 <div key={each.id}>
                     <ul className="listy">
                         <li key={each.name}><h2>Name:{each.name}</h2></li>
                             <p>Climate:{each.climate}</p>
                             <p>Terrain:{each.terrain}</p>
                     </ul>
+                </div>
                 </div>
             )
         })
@@ -49,9 +51,9 @@ class Locations extends Component {
                     <button onClick={this.hide} className="loc-btn" id="loc-btn">
                     {this.state.toggle ? "Hide Locations" : "Show Locations"}
                     </button>
-                    <button value="Name" className="loc-btn">Sort By Name</button>
-                    <button value="Climate" className="loc-btn">Sort By Climate</button>
-                    <button value="Terrain" className="loc-btn">Sort By Terrain</button>
+                    <button value="Name" className="loc-btn">{this.state.toggle ? "Sort By Name" : null}</button>
+                    <button value="Climate" className="loc-btn">{this.state.toggle ? "Sort By Climate" : null}</button>
+                    <button value="Terrain" className="loc-btn">{this.state.toggle ? "Sort By Terrain" : null}</button>
                     <ul>{this.state.toggle ? allLoco : null}</ul>
                 </div>
             </div>
