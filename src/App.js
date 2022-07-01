da67bce7bc749ec7debada14fb0b2a70d5b41893
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-07-01 16:28:43 -0300

    refactored code, submission version.

--- src/App.js
+++ src/App.js
@@ -6,38 +6,12 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import React from 'react';
 
-/* Destructured Fetches that I do for Movies, People, and Location portions keep returning undefined. Troubleshooted the problem online
-    And looked at past works but still couldn't find a fix with the time I had to work on this. 
-    At the very least other than the complications with Movie's dropbox I've coded at least the outline of the logic for People and Locations. */
-
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      searchedEntry: [],
-    };
+    this.state = {};
   }
-  fetchHandler = (searchInput) => {
-    let persons = [];
-    fetch(`https://ghibliapi.herokuapp.com/${searchInput}?format=j1`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const people = data.items;
-        persons = people.map((person) => {
-          return {
-            age: person.age,
-            eyes: person.eye_color,
-            hair: person.hair_color,
-          };
-        });
-        this.setState({ searchedEntry: persons });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+
   render() {
     return (
       <main>
